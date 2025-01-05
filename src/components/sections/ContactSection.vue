5d9338c7f354c599b61354c9bbf373caaeae1c3b
Gilles-Dereck GNANCADJA <gnancadjagillesdereck@gmail.com>
2025-01-05 10:16:02 +0100

    Personnalisation

--- src/components/sections/ContactSection.vue
+++ src/components/sections/ContactSection.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 
 const form = ref({
   name: '',
+  subject: '',
   email: '',
   message: ''
 });
@@ -22,10 +23,16 @@ const handleSubmit = () => {
       veuillez remplir et envoyer le formulaire en indiquant vos besoins. Merci !</p>
     <div class="max-w-2xl mx-auto">
       <div class="bg-gray-900/50 backdrop-blur-sm border border-white/10 rounded-lg p-8">
-        <form @submit.prevent="handleSubmit" class="space-y-6">
+        <form action="https://formsubmit.co/itgillo60@gmail.com" method="POST"  class="space-y-6">
           <div>
             <label for="name" class="block text-sm font-medium text-gray-300">Nom et Prenom</label>
             <input type="text" id="name" v-model="form.name"
+              class="mt-1 block w-full rounded-lg bg-gray-800 border-gray-700 text-white focus:ring-blue-500 focus:border-blue-500"
+              required />
+          </div>
+          <div>
+            <label for="subject" class="block text-sm font-medium text-gray-300">Sujet</label>
+            <input type="text" id="subject" v-model="form.subject"
               class="mt-1 block w-full rounded-lg bg-gray-800 border-gray-700 text-white focus:ring-blue-500 focus:border-blue-500"
               required />
           </div>
@@ -41,6 +48,9 @@ const handleSubmit = () => {
               class="mt-1 block w-full rounded-lg bg-gray-800 border-gray-700 text-white focus:ring-blue-500 focus:border-blue-500"
               required></textarea>
           </div>
+          <input type="hidden" name="_autoresponse" value="Merci de nous avoir contactés.
+                   Nous avons bien reçu votre requête et la traiterons dans les plus brefs délais. 
+                  Cordialement.">
           <button type="submit"
             class="w-full py-3 px-4 bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700 rounded-lg text-white font-medium transition-all duration-300 transform hover:scale-105">
             Envoyer le Message
